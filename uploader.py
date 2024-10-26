61a1fd06d41211dbd9aeefb426800b79133e7d27
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2024-10-26 15:30:09 +0300

    Get package name automatically

--- uploader.py
+++ uploader.py
@@ -5,18 +5,36 @@
 # ]
 # ///
 
+import argparse
+import json
+import re
 import sys
-import requests
+import zipfile
 from os import getenv
+from pathlib import Path
+
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument("zip_file", type=Path, help="Path to the zip file to upload")
+args = parser.parse_args()
 
 MOD_PORTAL_URL = "https://mods.factorio.com"
 INIT_UPLOAD_URL = f"{MOD_PORTAL_URL}/api/v2/mods/releases/init_upload"
 
 apikey = getenv("MOD_UPLOAD_API_KEY")
-modname = getenv("MOD_UPLOAD_NAME")
-zipfilepath = getenv("MOD_UPLOAD_FILE")
+zipfile_path: Path = args.zip_file
 
-request_body = data = {"mod": modname}
+with zipfile.ZipFile(zipfile_path) as z:
+    info_path = [name for name in z.namelist() if re.match(r"[^/]+/info.json", name)]
+    if len(info_path) != 1:
+        print(f"Expected to find a single info.json, but found {info_path}")
+        sys.exit(1)
+
+    info = json.loads(z.open(info_path[0]).read().decode())
+    mod_name = info["name"]
+
+request_body = data = {"mod": mod_name}
 request_headers = {"Authorization": f"Bearer {apikey}"}
 
 response = requests.post(INIT_UPLOAD_URL, data=request_body, headers=request_headers)
@@ -27,7 +45,7 @@ if not response.ok:
 
 upload_url = response.json()["upload_url"]
 
-with open(zipfilepath, "rb") as f:
+with zipfile_path.open("rb") as f:
     request_body = {"file": f}
     response = requests.post(upload_url, files=request_body)
 
